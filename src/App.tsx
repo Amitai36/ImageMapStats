7fbd20be2e0e301be9cf1ce6f699d5a3b356fd0d
amitai36 <Amitiy36@gmail.com>
2024-05-19 00:04:45 +0300

    add order files

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,30 @@
 import { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 
-import NavBar from "./components/layout/NavBar";
 import ProviderTheme from "./styles/CreateTheme";
-import PhotoStatistics from "./pages/statistics/photo";
-import UserPhotos from "./pages/userPhoto";
-const Home = lazy(() => import("./pages/home"));
+import NavBar from "./components/layout/NavBar";
+
+const PhotoStatistics = lazy(() => import("./pages/statistics/photo"));
 const UserStatistics = lazy(() => import("./pages/statistics/user"));
-const MapPage = lazy(() => import("./pages/map"));
+const UserPhotos = lazy(() => import("./pages/userPhoto"));
 const SearchRes = lazy(() => import("./pages/searchRes"));
+const MapPage = lazy(() => import("./pages/map"));
+const Home = lazy(() => import("./pages/home"));
 
 function App() {
   return (
     <ProviderTheme>
       <Routes>
         <Route path="/" element={<NavBar />}>
-          <Route path="/home" element={<Home />} />
+          <Route path="/userStatistics/:name" element={<UserStatistics />} />
+          <Route path="/userPhotos/:name" element={<UserPhotos />} />
           <Route path="/search" element={<SearchRes />} />
           <Route path="/map" element={<MapPage />} />
-          <Route path="/userStatistics/:name" element={<UserStatistics />} />
+          <Route path="/home" element={<Home />} />
           <Route
             path="/search/photoStatistics/:photoId"
             element={<PhotoStatistics />}
           />
-          <Route path="/userPhotos/:name" element={<UserPhotos />} />
         </Route>
       </Routes>
     </ProviderTheme>
